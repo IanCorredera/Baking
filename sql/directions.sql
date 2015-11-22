8e35e230156df72b86c500abc13f5ee93a9fb976
IanCorredera <ian.corredera@gmail.com>
2015-11-22 12:00:08 -0500

    sql inserts

--- sql/directions.sql
+++ sql/directions.sql
@@ -16,9 +16,16 @@ CREATE TABLE IF NOT EXISTS tblDirections (
     fldStep13 varchar(1000) DEFAULT NULL,
     fldStep14 varchar(1000) DEFAULT NULL,
     fldStep15 varchar(1000) DEFAULT NULL,
-    fldStep16 varchar(1000) DEFAULT NULL,
-    fldStep17 varchar(1000) DEFAULT NULL,
-    fldStep18 varchar(1000) DEFAULT NULL,
-    fldStep19 varchar(1000) DEFAULT NULL,
-    fldStep20 varchar(1000) DEFAULT NULL,
 )   ENGINE=InnoDB AUTO_INCREMENT=2228 DEFAULT CHARSET=utf8;
+
+INSERT INTO tblDirections (pmkRecipeName, fldStep1, fldStep2, fldStep3, fldStep4,
+fldStep5, fldStep6, fldStep7, fldStep8, fldStep9, fldStep10, fldStep11, fldStep12,
+fldStep13, fldStep14, fldStep15) VALUES
+('Lemon-Poppy Seed Bread','Whisk together the flours with the baking powder, baking soda, and salt in a medium bowl.',
+'Cream together the butter and sugar in a large mixing bowl until fluffy. Add the eggs, one at a time, scraping the sides and bottom of the bowl between additions. 
+Add the lemon zest, then one-third of the dry ingredients. Add the lemon juice, then another third of the dry ingredients. 
+Mix in the yogurt and poppy seeds, then the remaining dry ingredients.', 
+'Pour the batter into the prepared pan. Bake until a toothpick inserted in the center comes out clean, 1 hour to 1 hour 5minutes. 
+Remove from the oven, place on a rack, and cool in the pan for 15 to 20 minutes. After that time, run a table knife around the edge of the pan, and tip the bread out. 
+Return the bread to the rack to cool completely before glazing.');
+
--- sql/ingredients.sql
+++ sql/ingredients.sql
@@ -1,5 +1,20 @@
 DROP TABLE IF EXISTS tblIngredients;
 CREATE TABLE IF NOT EXISTS tblIngredients (
     pmkRecipeName varchar(255) NOT NULL PRIMARY KEY,
-    fldIngredient varchar(50) DEFAULT NULL,
+    fldIngredient varchar(255) DEFAULT NULL,
 )   ENGINE=InnoDB AUTO_INCREMENT=2228 DEFAULT CHARSET=utf8;
+
+INSERT INTO tblIngredients (pmkRecipeName, fldIngredient) VALUES
+('Lemon-Poppy Seed Bread', '1.5 cups white whole wheat flour'),
+('Lemon-Poppy Seed Bread', '1/2 cup oat flour'), 
+('Lemon-Poppy Seed Bread','3/4 cup unbleached all purpose flour'), 
+('Lemon-Poppy Seed Bread', '2 teaspoons baking powder'),
+('Lemon-Poppy Seed Bread', '1/2 teaspoon baking soda'),
+('Lemon-Poppy Seed Bread', '1/2 teaspoon salt'),
+('Lemon-Poppy Seed Bread', '1/2 cup unsalted butter'),
+('Lemon-Poppy Seed Bread', '1 cup sugar'),
+('Lemon-Poppy Seed Bread', '2 large eggs'),
+('Lemon-Poppy Seed Bread', '2 tablespoons grated lemon zest'),
+('Lemon-Poppy Seed Bread', '1/2 cup fresh lemon juice'),
+('Lemon-Poppy Seed Bread', '3/4 cup lemon yogurt'),
+('Lemon-Poppy Seed Bread', '1/4 cup poppy seeds');
