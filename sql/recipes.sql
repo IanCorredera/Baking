2f65ea8cf7caaa97c13121423973ce4196fe4bef
IanCorredera <ian.corredera@gmail.com>
2015-11-23 17:25:49 -0500

    fix?

--- sql/recipes.sql
+++ sql/recipes.sql
@@ -6,6 +6,5 @@ CREATE TABLE IF NOT EXISTS tblRecipes (
     fldCategory varchar(50) DEFAULT NULL
 )   
 
-INSERT INTO tblRecipes (pmkRecipeName, fldYield, fldBakeTime, fldCategory) VALUES
-('Lemon-Poppy Seed Bread', 'One 9 x 5-inch loaf', '1 hour', 'Quick Bread');
+INSERT INTO tblRecipes (pmkRecipeName, fldYield, fldBakeTime, fldCategory) VALUES ('Lemon Poppy Seed Bread', 'One 9 x 5-inch loaf', '1 hour', 'Quick Bread');
 
