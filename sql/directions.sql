d77ba97fcbb2b6f9234dcdab6cfe2f283611e2b9
IanCorredera <ian.corredera@gmail.com>
2015-11-23 18:08:05 -0500

    added null values

--- sql/directions.sql
+++ sql/directions.sql
@@ -21,11 +21,26 @@ CREATE TABLE IF NOT EXISTS tblDirections (
 INSERT INTO tblDirections (pmkRecipeName, fldStep1, fldStep2, fldStep3, fldStep4,
 fldStep5, fldStep6, fldStep7, fldStep8, fldStep9, fldStep10, fldStep11, fldStep12,
 fldStep13, fldStep14, fldStep15) VALUES
-('Lemon-Poppy Seed Bread','Whisk together the flours with the baking powder, baking soda, and salt in a medium bowl.',
-'Cream together the butter and sugar in a large mixing bowl until fluffy. Add the eggs, one at a time, scraping the sides and bottom of the bowl between additions. 
-Add the lemon zest, then one-third of the dry ingredients. Add the lemon juice, then another third of the dry ingredients. 
+('Lemon-Poppy Seed Bread',
+'Whisk together the flours with the baking powder, baking soda, and salt in a medium bowl.',
+'Cream together the butter and sugar in a large mixing bowl until fluffy. Add the eggs, one 
+at a time, scraping the sides and bottom of the bowl between additions. 
+Add the lemon zest, then one-third of the dry ingredients. Add the lemon juice, 
+then another third of the dry ingredients. 
 Mix in the yogurt and poppy seeds, then the remaining dry ingredients.', 
 'Pour the batter into the prepared pan. Bake until a toothpick inserted in the center comes out clean, 1 hour to 1 hour 5minutes. 
 Remove from the oven, place on a rack, and cool in the pan for 15 to 20 minutes. After that time, run a table knife around the edge of the pan, and tip the bread out. 
-Return the bread to the rack to cool completely before glazing.');
+Return the bread to the rack to cool completely before glazing.', 
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL,
+NULL);
 
