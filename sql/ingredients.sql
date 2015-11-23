b3608bcc4963d7785a971b1721aff42eec46f227
IanCorredera <ian.corredera@gmail.com>
2015-11-22 21:03:50 -0500

    update

--- sql/ingredients.sql
+++ sql/ingredients.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS tblIngredients;
 CREATE TABLE IF NOT EXISTS tblIngredients (
-    pmkRecipeName varchar(255) NOT NULL PRIMARY KEY,
+    pmkRecipeName varchar(255) DEFAULT NULL,
     fldIngredient varchar(255) DEFAULT NULL,
 )   ENGINE=InnoDB AUTO_INCREMENT=2228 DEFAULT CHARSET=utf8;
 
