1c573b91e6241a67b5cffc29a06142d3a575b5e2
IanCorredera <ian.corredera@gmail.com>
2015-11-24 22:55:15 -0500

    sql fix

--- select.php
+++ select.php
@@ -12,11 +12,11 @@ include "top.php";
 
 print '<table>';
 
-$query = "select tblRecipes.pmkRecipeName, fldYield, fldBakeTime, fldIngredient, fldStep1, fldStep2, fldStep3 
+$query = "select tblRecipes.pmkRecipeName, fldYield, fldBakeTime, fldIngredient1, fldIngredient2, fldIngredient3, fldIngredient4, fldIngredient5, fldIngredient6, fldIngredient7, fldIngredient8, fldIngredient9, fldIngredient10, fldIngredient11, fldIngredient12, fldIngredient13, fldStep1, fldStep2, fldStep3 
 from tblRecipes, tblIngredients, tblDirections
 where tblRecipes.pmkRecipeName = tblIngredients.pmkRecipeName 
 and tblIngredients.pmkRecipeName = tblDirections.pmkRecipeName";
-    $columns = 7;
+    $columns = 13;
     
     $info2 = $thisDatabaseReader->testquery($query, "", 0, 0, 0, 0, false, false);
     $info2 = $thisDatabaseReader->select($query, "", 0, 0, 0, 0, false, false);
--- sql/ingredients.sql
+++ sql/ingredients.sql
@@ -1,20 +1,25 @@
 DROP TABLE IF EXISTS tblIngredients;
 CREATE TABLE IF NOT EXISTS tblIngredients (
     pmkRecipeName varchar(255) DEFAULT NULL,
-    fldIngredient varchar(255) DEFAULT NULL
+    fldIngredient1 varchar(255) DEFAULT NULL,
+    fldIngredient2 varchar(255) DEFAULT NULL,
+    fldIngredient3 varchar(255) DEFAULT NULL,
+    fldIngredient4 varchar(255) DEFAULT NULL,
+    fldIngredient5 varchar(255) DEFAULT NULL,
+    fldIngredient6 varchar(255) DEFAULT NULL,
+    fldIngredient7 varchar(255) DEFAULT NULL,
+    fldIngredient8 varchar(255) DEFAULT NULL,
+    fldIngredient9 varchar(255) DEFAULT NULL,
+    fldIngredient10 varchar(255) DEFAULT NULL,
+    fldIngredient11 varchar(255) DEFAULT NULL,
+    fldIngredient12 varchar(255) DEFAULT NULL,
+    fldIngredient13 varchar(255) DEFAULT NULL
 );
 
-INSERT INTO tblIngredients (pmkRecipeName, fldIngredient) VALUES
-('Lemon-Poppy Seed Bread', '1.5 cups white whole wheat flour'),
-('Lemon-Poppy Seed Bread', '1/2 cup oat flour'), 
-('Lemon-Poppy Seed Bread','3/4 cup unbleached all purpose flour'), 
-('Lemon-Poppy Seed Bread', '2 teaspoons baking powder'),
-('Lemon-Poppy Seed Bread', '1/2 teaspoon baking soda'),
-('Lemon-Poppy Seed Bread', '1/2 teaspoon salt'),
-('Lemon-Poppy Seed Bread', '1/2 cup unsalted butter'),
-('Lemon-Poppy Seed Bread', '1 cup sugar'),
-('Lemon-Poppy Seed Bread', '2 large eggs'),
-('Lemon-Poppy Seed Bread', '2 tablespoons grated lemon zest'),
-('Lemon-Poppy Seed Bread', '1/2 cup fresh lemon juice'),
-('Lemon-Poppy Seed Bread', '3/4 cup lemon yogurt'),
-('Lemon-Poppy Seed Bread', '1/4 cup poppy seeds');
+INSERT INTO tblIngredients (pmkRecipeName, fldIngredient1, fldIngredient2, fldIngredient3, fldIngredient4, fldIngredient5, fldIngredient6, fldIngredient7, fldIngredient8, fldIngredient9, fldIngredient10, fldIngredient11, fldIngredient12, fldIngredient13) VALUES
+('Lemon-Poppy Seed Bread', '1.5 cups white whole wheat flour', 
+'1/2 cup oat flour', '3/4 cup unbleached all purpose flour', 
+'2 teaspoons baking powder', '1/2 teaspoon baking soda', '1/2 teaspoon salt', 
+'1/2 cup unsalted butter', '1 cup sugar', '2 large eggs', '2 tablespoons grated lemon zest',
+'1/2 cup fresh lemon juice', '3/4 cup lemon yogurt', '1/4 cup poppy seeds');
+
