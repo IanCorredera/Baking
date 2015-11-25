0ce79dfa01c20c81b9b11435ed23553c03f6f45f
IanCorredera <ian.corredera@gmail.com>
2015-11-24 22:01:43 -0500

    css changes

--- header.php
+++ header.php
@@ -1,12 +1,10 @@
 <!-- %%%%%%%%%%%%%%%%%%%%%%     Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
 
 <header>
-    <figure class="float_left small">
-            <img alt="calvin and hobbes has influenced my snowman making." src="images/snowmen.jpg">
-            <figcaption>Snowmen observing an accident.</figcaption>
-    </figure>
-    <h1>UVM Course Enrollment</h1>
-    <h2>Fall 2015</h2>
+    
+    </figure
+    <h1>Baking Recipes</h1>
+    
 </header>
 
 <!-- %%%%%%%%%%%%%%%%%%%%% Ends Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
